17091435a91bc989eee81c54fb52390b1f282c75
hiroya-sarwakame <kame.2146@gmail.com>
2020-08-20 17:54:36 +0900

    残業申請のモーダル表示まで

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
   resources :users do
     member do
       get 'edit_basic_info'
+      get 'create_overtime'
       patch 'update_basic_info'
       get 'attendances/edit_one_month'
       patch 'attendances/update_one_month'
+      # patch 'attendances/create_overtime'
     end
     resources :attendances, only: :update
   end
